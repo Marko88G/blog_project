08f9153da29dfff7b6fd67c19582bf90dacd174f
Marko Grahovac <marko.grahovac7@gmail.com>
2021-10-21 16:59:44 +0200

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid, blanditiis consequatur dignissimos dolor dolorum enim, id, ipsam itaque iusto minus nam nostrum nulla placeat quidem sint sunt ut voluptatibus.</span><span>Asperiores autem beatae eos explicabo labore laboriosam nobis velit veniam vero voluptate! Accusantium aliquam, consequatur deleniti distinctio, iusto molestiae neque porro quaerat quas quis quo suscipit tempora, temporibus vel voluptatem!</span><span>Alias commodi consequatur consequuntur esse facilis incidunt ipsa labore, maxime nobis pariatur porro ratione tenetur unde? Asperiores enim error, esse et, minus nesciunt nulla numquam perferendis quam, similique tempore ullam?</span>
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">My Second Post</a></h1>
+    <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid, blanditiis consequatur dignissimos dolor dolorum enim, id, ipsam itaque iusto minus nam nostrum nulla placeat quidem sint sunt ut voluptatibus.</span><span>Asperiores autem beatae eos explicabo labore laboriosam nobis velit veniam vero voluptate! Accusantium aliquam, consequatur deleniti distinctio, iusto molestiae neque porro quaerat quas quis quo suscipit tempora, temporibus vel voluptatem!</span><span>Alias commodi consequatur consequuntur esse facilis incidunt ipsa labore, maxime nobis pariatur porro ratione tenetur unde? Asperiores enim error, esse et, minus nesciunt nulla numquam perferendis quam, similique tempore ullam?</span>
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">My Third Post</a></h1>
+    <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid, blanditiis consequatur dignissimos dolor dolorum enim, id, ipsam itaque iusto minus nam nostrum nulla placeat quidem sint sunt ut voluptatibus.</span><span>Asperiores autem beatae eos explicabo labore laboriosam nobis velit veniam vero voluptate! Accusantium aliquam, consequatur deleniti distinctio, iusto molestiae neque porro quaerat quas quis quo suscipit tempora, temporibus vel voluptatem!</span><span>Alias commodi consequatur consequuntur esse facilis incidunt ipsa labore, maxime nobis pariatur porro ratione tenetur unde? Asperiores enim error, esse et, minus nesciunt nulla numquam perferendis quam, similique tempore ullam?</span>
+    </p>
+</article>
+</body>
+</html>
